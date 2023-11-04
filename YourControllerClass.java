0e91eb372a2f1b22b5f6182a6cc85c24f6fca249
Sor2n <143337184+Sor2n@users.noreply.github.com>
2023-11-04 20:46:59 +0400

    Add files via upload

---
+++ YourControllerClass.java
@@ -0,0 +1,15 @@
+package mressa.ml.mst;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+public class YourControllerClass {
+    @FXML
+    private Button yourButton;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Hello World!");
+    }
+}
